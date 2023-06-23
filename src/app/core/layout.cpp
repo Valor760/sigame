afcf00a2ebab97357741fb005c755e25f4baeef4
valor760 <valorcool@gmail.com>
2023-06-23 19:30:23 +0300

    Added a check for presence of all fields for buttons and windows

--- src/app/core/layout.cpp
+++ src/app/core/layout.cpp
@@ -49,10 +49,55 @@ static Button* fill_button_item(const json& button)
 {
 	/* FIXME: Replace everything with try-catch, if the field is abscent */
 	Button* bt = new Button();
-	bt->Label = GET_JSON_STR(button["Label"]);
-	bt->Size = GET_JSON_VEC2(button["Size"]);
-	bt->Position = GET_JSON_VEC2(button["Position"]);
-	bt->pButtonPressedCallback = (button_callback_t) m_ButtonCallbackMap[GET_JSON_STR(button["PressedCallbackName"])];
+
+	if(button.contains("Label"))
+	{
+		bt->Label = GET_JSON_STR(button["Label"]);
+	}
+	else
+	{
+		LOG_ERROR("Button doesn't have \"%s\" field!", "Label");
+		return nullptr;
+	}
+
+	if(button.contains("Size"))
+	{
+		bt->Size = GET_JSON_VEC2(button["Size"]);
+	}
+	else
+	{
+		LOG_ERROR("Button \"%s\" doesn't have \"%s\" field. Don't know what size to choose.", bt->Label.c_str(), "Size");
+		return nullptr;
+	}
+
+	if(button.contains("Position"))
+	{
+		bt->Position = GET_JSON_VEC2(button["Position"]);
+	}
+	else
+	{
+		bt->Position = ImVec2(0, 0);
+		LOG_INFO("Button \"%s\" doesn't have \"%s\" field. Defaulting to (%d, %d)",
+			bt->Label.c_str(), "Position", (int)bt->Position.x, (int)bt->Position.y);
+	}
+
+	if(button.contains("PressedCallbackName"))
+	{
+		try
+		{
+			bt->pButtonPressedCallback = (button_callback_t) m_ButtonCallbackMap[GET_JSON_STR(button["PressedCallbackName"])];
+		}
+		catch(...)
+		{
+			bt->pButtonPressedCallback = nullptr;
+			LOG_ERROR("Button \"%s\": Can't find \"%s\" callback function.");
+		}
+	}
+	else
+	{
+		LOG_ERROR("Button \"%s\" doesn't have \"%s\" field. Why such useless button exists?",
+			bt->Label.c_str(), "PressedCallbackName");
+	}
 
 	return bt;
 }
@@ -86,23 +131,72 @@ bool LayoutManager::applyLayout(const json& layout_data)
 
 	for(auto& window : layout_data["Windows"])
 	{
-		LayoutWindow w = {
-			/* FIXME: Add constants/defines for json entries */
-			.Label = GET_JSON_STR(window["Label"]),
-			.Size = GET_JSON_VEC2(window["Size"]),
-			.Position = GET_JSON_VEC2(window["Position"]),
-			/* TODO: Add suitable flag parsing for flags */
-			.Flags = GET_JSON_INT(window["Flags"]),
-		};
+		/* FIXME: Add constants/defines for json entries */
+		LayoutWindow w;
 
-		/* Add items */
-		if(window["Items"].size())
+		/* Unfortunately, json library crashes with assert if the element is not present, so can't use try-catch */
+		/*
+			TODO?: I can rewrite that block of code to not use assertion, but this
+			will mean, that we cannot update this library at all.
+		*/
+		if(window.contains("Label"))
 		{
-			for(auto& item : window["Items"])
+			w.Label = GET_JSON_STR(window["Label"]);
+		}
+		else
+		{
+			LOG_ERROR("Window doesn't contain \"Label\" field");
+			return false;
+		}
+
+		if(window.contains("Size"))
+		{
+			w.Size = GET_JSON_VEC2(window["Size"]);
+		}
+		else
+		{
+			w.Size = Window::GetSize();
+			LOG_INFO("Window \"%s\" doesn't have \"%s\" field. Defaulting to window size (%d, %d)",
+				w.Label.c_str(), "Size", (int)w.Size.x, (int)w.Size.y);
+		}
+
+		if(window.contains("Position"))
+		{
+			w.Position = GET_JSON_VEC2(window["Position"]);
+		}
+		else
+		{
+			w.Position = ImVec2(0, 0);
+			LOG_INFO("Window \"%s\" doesn't have \"%s\" field. Defaulting to (%d, %d)",
+				w.Label.c_str(), "Position", (int)w.Position.x, (int)w.Position.y);
+		}
+
+		if(window.contains("Flags"))
+		{
+			/* TODO: Add suitable flag parsing for flags */
+			w.Flags = GET_JSON_INT(window["Flags"]);
+		}
+		else
+		{
+			w.Flags = 0;
+			LOG_INFO("Window \"%s\" doesn't have \"%s\" field. Defaulting to %d",
+				w.Label.c_str(), "Flags", w.Flags);
+		}
+
+		if(window.contains("Items"))
+		{
+			auto items = window["Items"];
+			for(auto& item : items)
 			{
 				w.Items.push_back(fill_item(item));
 			}
 		}
+		else
+		{
+			LOG_INFO("Window \"%s\" doesn't have \"%s\" field. Array will be empty.", 
+				w.Label.c_str(), "Items");
+		}
+
 		m_CurrentLayoutStack.push_back(w);
 	}
 
@@ -152,7 +246,10 @@ void draw_button(Button* button)
 	ImGui::SetCursorPos(button->Position);
 	if(ImGui::Button(button->Label.c_str(), button->Size))
 	{
-		button->pButtonPressedCallback();
+		if(button->pButtonPressedCallback)
+		{
+			button->pButtonPressedCallback();
+		}
 	}
 }
 
